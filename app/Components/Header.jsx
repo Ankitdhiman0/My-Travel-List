faf06fb216ffde44615fed1e121053e7d9d4468c
Ankit Dhiman <ankitdhiman4567@gmail.com>
2024-09-18 14:35:49 +0530

    new changes

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="Logo w-1/2 h-28 flex flex-col justify-center items-center">
           <div className="flex items-center gap-5">
             <img src="logo.png" className="h-24" />
-            <h1 className=" head text-5xl">My Travel List</h1>
+            <h1 className="text-5xl">My Travel List</h1>
             <img src="bags.png" className="h-24" />
           </div>
         </div>
