f32b10eddb41793a87384d7f3f726b8674f5598a
Ankit Dhiman <ankitdhiman4567@gmail.com>
2024-09-18 14:34:20 +0530

    added header

---
+++ app/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header w-full h-32 flex justify-center items-center">
+        <div className="Logo w-1/2 h-28 flex flex-col justify-center items-center">
+          <div className="flex items-center gap-5">
+            <img src="logo.png" className="h-24" />
+            <h1 className=" head text-5xl">My Travel List</h1>
+            <img src="bags.png" className="h-24" />
+          </div>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+};
+
+export default Header;
